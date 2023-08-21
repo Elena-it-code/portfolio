5f9d94bd717e865c51b7bd34c7960aab306f199c
ElenaKuklina <cuklina.lena@gmail.com>
2023-08-21 22:38:21 +0300

    Add idea to ignor

---
+++ src/layout/section/about/About.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SectionTitle} from "../../../components/SectionTitle";
+import {SubTitle} from "../../../components/SubTitle";
+import styled from "styled-components";
+
+
+export const About = () => {
+    return (
+        <StyledSectionAbout>
+            <SectionTitle>About me</SectionTitle>
+            <SubTitle>Hi, I'm Denis – UX/UI designer from Minsk. I'm interested in design and everything connected with it. I'm studying at courses "Web and mobile design interfaces" in IT-Academy. Ready to implement excellent projects with wonderful people.</SubTitle>
+        </StyledSectionAbout>
+    );
+};
+
+const StyledSectionAbout = styled.section`
+  background-color: rgba(50, 73, 220, 0.31);
+  min-height: 100vh;
+`
+
+// вместо section выдает ошибку TS1005 , EXPETED
