a8624a14c798ffed63569516e265ddb7c5a011ee
ElenaKuklina <cuklina.lena@gmail.com>
2023-09-02 00:15:51 +0300

    Add styles for section About me

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -2,4 +2,6 @@ import styled from "styled-components";
 
 export const SectionTitle = styled.h2`
   text-align: center;
+  padding-top: 120px;
+  margin-bottom: 30px;
 `
--- src/layout/section/about/About.tsx
+++ src/layout/section/about/About.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import {SectionTitle} from "../../../components/SectionTitle";
 import {SubTitle} from "../../../components/SubTitle";
 import styled from "styled-components";
+import {Container} from "../../../components/Container";
 
 
 export const About = () => {
     return (
         <StyledSectionAbout>
-            <SectionTitle>About me</SectionTitle>
-            <SubTitle>Hi, I'm Denis – UX/UI designer from Minsk. I'm interested in design and everything connected with it. I'm studying at courses "Web and mobile design interfaces" in IT-Academy. Ready to implement excellent projects with wonderful people.</SubTitle>
-        </StyledSectionAbout>
+            <Container>
+                <SectionTitle>About me</SectionTitle>
+                    <SubTitle>Hi, I'm Denis – UX/UI designer from Minsk.<br/>I'm interested in design and everything connected with it.</SubTitle>
+                        <SubTitle>I'm studying at courses "Web and mobile design<br/>interfaces" in IT-Academy.</SubTitle>
+                            <SubTitle>Ready to implement excellent projects<br/>with wonderful people.</SubTitle>
+                </Container>
+            </StyledSectionAbout>
     );
 };
 
 const StyledSectionAbout = styled.section`
-  min-height: 100vh;
+  min-height: 500px;
 `
 
 // вместо section выдает ошибку TS1005 , EXPETED
