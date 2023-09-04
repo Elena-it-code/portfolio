3837ae13d7de1595588c087ea8196462481cdd3a
ElenaKuklina <cuklina.lena@gmail.com>
2023-09-04 12:00:50 +0300

    Add styles for section Title

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const SectionTitle = styled.h2`
+  font-size: 34px;
   text-align: center;
-  padding-top: 130px;
+  padding-top: 120px;
   margin-bottom: 30px;
 `
