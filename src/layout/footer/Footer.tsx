dc937e059e18c2d7f3cdf5d68338d867ca4f51f0
ElenaKuklina <cuklina.lena@gmail.com>
2023-09-05 16:22:57 +0300

    Add styles for section Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import styled from "styled-components";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
     return (
         <StyledFooter>
-<StyledCopyright>Like me on LinkedIn, Instagram, Behance, Dribble</StyledCopyright>
+<StyledCopyright>Like me on<br/>LinkedIn, Instagram, Behance, Dribble</StyledCopyright>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
-display: flex;
+  display: flex;
   justify-content: center;
+  margin-top: -90px;
+  margin-bottom: 120px;
 `
 const StyledCopyright = styled.small`
-
+  text-align: center;
+  font-family: Gilroy-Medium, sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${theme.colors.secondaryBg};
 `
